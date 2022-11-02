5b90daefcd551ae9a5ab0cebec96c465273bd39c
Henrique-Marmiroli <lhm.ambiental@gmail.com>
2022-11-02 18:14:53 -0300

    libs banco de dados

--- Landing_Replayers/settings.py
+++ Landing_Replayers/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import dj_database_url
+
+DATABASES = {
+    'default': dj_database_url.config()
+}
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -79,13 +84,6 @@ WSGI_APPLICATION = 'Landing_Replayers.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
 
 
 # Password validation
